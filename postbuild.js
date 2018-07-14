38cf6c582f4521fd5851e9d6e09bc8f2200a1588
Jesse Lawson (MBP) <jesse@lawsonry.com>
2018-07-14 14:15:31 -0700

    Update postbuild; remove version file; read version directly from package.json; build dist

--- postbuild.js
+++ postbuild.js
@@ -6,18 +6,14 @@ const file = "dist/campfire.min.js"
 
 var fs = require('fs'),
 ERROR_EMPTY = 'could not be empty';
-var currentTag = ""
-
-fs.readFile('VERSION', 'utf8', function(err, contents) {
-    currentTag = contents
-});
+currentTag = require('./package').version
 
 fs.readFile(file, 'utf8', function(error, result) {
     if (error && error.code !== 'ENOENT') {
         console.log("Error doing postbuild.")
     } else {
         if (result)
-        var banner = "/* "+details+" ("+currentTag.trim()+") */"
+        var banner = "/* "+details+" (v"+currentTag.trim()+") */"
             banner = banner + result;
         
         fs.writeFile(file, banner, function(error){ if(error){console.log("Error writing to built file.")}});
